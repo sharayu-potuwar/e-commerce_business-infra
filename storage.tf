53fc8251a0d7e1ae67a4a417303edc2e82338cfd
sharayu-potuwar <sharayu.potuwar@gmail.com>
2024-04-15 21:54:46 -0400

    created tfvars file

--- storage.tf
+++ storage.tf
@@ -1,9 +1,9 @@
-resource "random_string" "db-password" {
-  length  = 32
-  upper   = true
-  numeric = true
-  special = false
-}
+# resource "random_string" "db-password" {
+#   length  = 32
+#   upper   = true
+#   numeric = true
+#   special = false
+# }
 
 
 resource "aws_db_instance" "registration" {
@@ -13,7 +13,7 @@ resource "aws_db_instance" "registration" {
     engine                 = "postgres"
     engine_version         = "16.1"
     username               = "ecom_user"
-    password               = var.db_password.value
+    password               = var.db_password
     publicly_accessible    = true
     skip_final_snapshot    = true
 }
