0e383766512d21ab1435a1309fd3856445da98a4
sharayu-potuwar <sharayu.potuwar@gmail.com>
2024-04-19 18:01:32 -0400

    provisioned ECS service

--- service.tf
+++ service.tf
@@ -1,14 +1,20 @@
 resource "aws_ecs_service" "main" {
- name                               = "e-comm_ecs_service"
- cluster                            = aws_ecs_cluster.main.id
- task_definition                    = aws_ecs_task_definition.e-comm_ecs_task.arn
- desired_count                      = 2
- deployment_minimum_healthy_percent = 50
- deployment_maximum_percent         = 200
- launch_type                        = "FARGATE"
- scheduling_strategy                = "REPLICA"
- 
- lifecycle {
-   ignore_changes = [task_definition, desired_count]
- }
+    name                               = "e-comm_ecs_service"
+    cluster                            = aws_ecs_cluster.main.id
+    task_definition                    = aws_ecs_task_definition.e-comm_ecs_task.arn
+    desired_count                      = 2
+    deployment_minimum_healthy_percent = 50
+    deployment_maximum_percent         = 200
+    launch_type                        = "FARGATE"
+    scheduling_strategy                = "REPLICA"
+
+    network_configuration {
+        security_groups  = "sg-0070f32bfa40490c2"
+        subnets          = "subnet-005a5c83ac0837b4f"
+        assign_public_ip = true
+    }
+
+    lifecycle {
+        ignore_changes = [task_definition, desired_count]
+    }
 }
