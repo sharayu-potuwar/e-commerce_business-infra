4ac7cbf0f7d4db7ad82d53be1373b23b02881dd7
sharayu-potuwar <sharayu.potuwar@gmail.com>
2024-04-19 14:03:36 -0400

    Provisioned ECR lifecycle policy

--- repository.tf
+++ repository.tf
@@ -5,3 +5,22 @@ resource "aws_ecr_repository" "ecom_repo" {
     scan_on_push = true
   }
 }
+
+resource "aws_ecr_lifecycle_policy" "ecom_repo_policy" {
+  repository = aws_ecr_repository.ecom_repo.name
+ 
+  policy = jsonencode({
+   rules = [{
+     rulePriority = 1
+     description  = "keep last 10 images"
+     action       = {
+       type = "expire"
+     }
+     selection     = {
+       tagStatus   = "any"
+       countType   = "imageCountMoreThan"
+       countNumber = 10
+     }
+   }]
+  })
+}
