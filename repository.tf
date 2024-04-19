cc3a6ad30facec66503eb84b7d04224ab56b50a8
sharayu-potuwar <sharayu.potuwar@gmail.com>
2024-04-19 00:08:02 -0400

    ecom_repo reinstated

--- repository.tf
+++ repository.tf
@@ -1,5 +1,5 @@
-resource "aws_ecr_repository" "my_ecr_repo" {
-  name = "my-ecr-repo"          # Name of the repository
+resource "aws_ecr_repository" "ecom_repo" {
+  name = "ecom_repo"          # Name of the repository
   image_tag_mutability = "MUTABLE"
   image_scanning_configuration {
     scan_on_push = true
