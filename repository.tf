a39c2a39c07445ab5dd0937d706683037d78ff87
sharayu-potuwar <sharayu.potuwar@gmail.com>
2024-04-19 00:04:37 -0400

    simplify ecr repo creation

--- repository.tf
+++ repository.tf
@@ -1,32 +1,7 @@
-resource "aws_ecr_repository" "repo" {
-  name                 = "e-commerce-repo"
+resource "aws_ecr_repository" "my_ecr_repo" {
+  name = "my-ecr-repo"          # Name of the repository
   image_tag_mutability = "MUTABLE"
-
   image_scanning_configuration {
     scan_on_push = true
   }
-}
-
-data "aws_iam_policy_document" "policy_doc" {
-  statement {
-    sid    = "allowecs"
-    effect = "Allow"
-    principals {
-        type = "service"
-        identifiers = ["ecs.amazonaws.com"]
-    }
-    actions = [
-        "ecr:BatchGetImage",
-        "ecr:GetDownloadUrlForLayer",
-        "ecr:GetAuthorizationToken"
-    ]
-    resources = [
-        "arn:aws:ecr:::*"
-    ]
-  }
-}
-
-resource "aws_ecr_repository_policy" "repo_policy" {
-  repository = aws_ecr_repository.repo.name
-  policy     = data.aws_iam_policy_document.policy_doc.json
 }
