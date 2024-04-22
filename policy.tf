08d25746ea2d5eb91a173f873950610cc8b9face
sharayu-potuwar <sharayu.potuwar@gmail.com>
2024-04-22 13:45:03 -0400

    provisioned aws_ecr_repository_policy

--- policy.tf
+++ policy.tf
@@ -2,3 +2,39 @@ resource "aws_iam_role_policy_attachment" "ecs-task-execution-role-policy-attach
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
+# resource "aws_iam_policy" "policy" {
+#   name        = "test_policy"
+#   path        = "/"
+#   description = "My test policy"
+
+#   # Terraform's "jsonencode" function converts a
+#   # Terraform expression result to valid JSON syntax.
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       { 
+#          "Sid":"AllowPull",
+#          "Effect":"Allow",
+#          "Principal":{
+#             "Service":"ecs-tasks.amazonaws.com"
+#          }
+#         Action = [
+#           "ecr:BatchGetImage",
+#           "ecr:GetDownloadUrlForLayer",
+#         ]
+#         Effect   = "Allow"
+#         Resource = "*"
+#       }
+#     ]
+#   })
+# }
+
+resource "aws_ecr_repository_policy" "example" {
+  repository = aws_ecr_repository.ecom_repo.name
+  policy = aws_ecr_lifecycle_policy.ecom_repo_policy.policy
+  depends_on = [ aws_iam_role.ecs_task_execution_role  ]     
+  }
+
+
+
