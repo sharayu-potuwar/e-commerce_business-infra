a73bc5b1d1243d20ca6926248cf04cb8d842e871
sharayu-potuwar <sharayu.potuwar@gmail.com>
2024-04-23 17:37:18 -0400

    commented the target type

--- target_group.tf
+++ target_group.tf
@@ -1,6 +1,6 @@
 resource "aws_lb_target_group" "ecom_target" {
   name        = "tf-alb-tg"
-  target_type = "alb"
+#   target_type = "alb"
   port        = 80
   protocol    = "HTTP"
   vpc_id      = "vpc-05b1ca67a2465043e"
