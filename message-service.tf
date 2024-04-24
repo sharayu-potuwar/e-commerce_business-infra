78b6a33022459acda37aa7ae39c4cb2994eb5260
sharayu-potuwar <sharayu.potuwar@gmail.com>
2024-04-24 14:43:13 -0400

    configured policy for sns

--- message-service.tf
+++ message-service.tf
@@ -1,3 +1,49 @@
 resource "aws_sns_topic" "ecom-sns" {
   name = "ecom-user-updates-topic"
 }
+
+resource "aws_sns_topic_policy" "default" {
+  arn = aws_sns_topic.ecom-sns.arn
+
+  policy = data.aws_iam_policy_document.sns_topic_policy.json
+}
+
+data "aws_iam_policy_document" "sns_topic_policy" {
+  policy_id = "__default_policy_ID"
+
+  statement {
+    actions = [
+      "SNS:Subscribe",
+      "SNS:SetTopicAttributes",
+      "SNS:RemovePermission",
+      "SNS:Receive",
+      "SNS:Publish",
+      "SNS:ListSubscriptionsByTopic",
+      "SNS:GetTopicAttributes",
+      "SNS:DeleteTopic",
+      "SNS:AddPermission",
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceOwner"
+
+      values = [
+        "211125373436",
+      ]
+    }
+
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    resources = [
+      aws_sns_topic.ecom-sns.arn,
+    ]
+
+    sid = "__default_statement_ID"
+  }
+}
