12087ecbd09c38d016898d6bef9336691c8e5051
sharayu-potuwar <sharayu.potuwar@gmail.com>
2024-05-28 18:24:44 -0400

    S3 resource provisioned

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -1,16 +1,16 @@
-data "aws_s3_object" "bucketreference" {
-  bucket = "ecom-email-sourcecode-bucket"
-  key    = "campaign_service.zip"
-}
+# data "aws_s3_object" "bucketreference" {
+#   bucket = "ecom-email-sourcecode-bucket"
+#   key    = "campaign_service.zip"
+# }
 
-resource "aws_lambda_function" "ecom_lambda" {
-    s3_bucket         = data.aws_s3_object.bucketreference.bucket
-    s3_key            = data.aws_s3_object.bucketreference.key
-    s3_object_version = data.aws_s3_object.bucketreference.version_id
-    function_name     = "e-commerce_business-campaign-service"
-    role              = aws_iam_role.iam_for_lambda.arn # (not shown)
-    handler           = "welcome-mail.lambda_handler"
-    runtime           = "python3.10"
-}
+# resource "aws_lambda_function" "ecom_lambda" {
+#     s3_bucket         = data.aws_s3_object.bucketreference.bucket
+#     s3_key            = data.aws_s3_object.bucketreference.key
+#     s3_object_version = data.aws_s3_object.bucketreference.version_id
+#     function_name     = "e-commerce_business-campaign-service"
+#     role              = aws_iam_role.iam_for_lambda.arn # (not shown)
+#     handler           = "welcome-mail.lambda_handler"
+#     runtime           = "python3.10"
+# }
 
 
